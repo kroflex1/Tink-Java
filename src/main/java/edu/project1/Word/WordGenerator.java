5594c824744a2fb7c7d7f9f99a18f4cf7d3ba409
Artyom Sokolov <15012003artem@mail.ru>
2023-10-21 18:53:48 +0500

    Update WordGenerator and add tests

--- src/main/java/edu/project1/Word/WordGenerator.java
+++ src/main/java/edu/project1/Word/WordGenerator.java
@@ -12,16 +12,22 @@ import java.util.Set;
 import static java.util.Map.entry;
 
 public class WordGenerator {
-    private static final Map<Topic, Set<String>> words = new HashMap<>();
-    private static final Random randomizer = new Random();
+    private final Map<Topic, Set<String>> words;
+    private final Random randomizer;
+
+    public WordGenerator() {
+        words = new HashMap<>();
+        fillTopicsWithWords();
+        randomizer = new Random();
+    }
 
     @NotNull
-    public static String getRandomWordFromTopic(Topic topic) throws IllegalArgumentException {
+    public String getRandomWordFromTopic(Topic topic) throws IllegalArgumentException {
         if (!words.containsKey(topic)) {
             throw new IllegalArgumentException("The submitted topic is not available");
         }
         Set<String> topicWords = words.get(topic);
-        int randomIndex = randomizer.nextInt(words.size());
+        int randomIndex = randomizer.nextInt(topicWords.size());
         int i = 0;
         for (String word : topicWords) {
             if (i == randomIndex) {
@@ -32,34 +38,30 @@ public class WordGenerator {
         throw new IllegalStateException("Something went wrong while picking a random word");
     }
 
-    public static void addNewWordToTopic(String word, Topic topic) {
+    public void addNewWordToTopic(String word, Topic topic) {
         if (!words.containsKey(topic)) {
             words.put(topic, new HashSet<>());
         }
         words.get(topic).add(word);
     }
 
-    public static void addWordsToTopic(Set<String> newWords, Topic topic) {
+    public void addWordsToTopic(Set<String> newWords, Topic topic) {
         if (!words.containsKey(topic)) {
             words.put(topic, newWords);
         }
         words.get(topic).addAll(newWords);
     }
 
-    public static Set<Topic> getAvailableTopics() {
+    public Set<Topic> getAvailableTopics() {
         return words.keySet();
     }
 
-    private static void fillTopicsWithWords() {
+    private void fillTopicsWithWords() {
         Set<String> sportWords = new HashSet<>(Arrays.asList("football", "basketball", "hockey"));
         Set<String> animalWords = new HashSet<>(Arrays.asList("lion", "wolf", "rabbit"));
         Set<String> foodWords = new HashSet<>(Arrays.asList("apple", "cake", "carrot"));
         addWordsToTopic(sportWords, Topic.Sport);
         addWordsToTopic(animalWords, Topic.Animals);
         addWordsToTopic(foodWords, Topic.Food);
-    }
-
-    static {
-        fillTopicsWithWords();
     }
 }
--- src/test/java/edu/hw1/project1/WordGeneratorTest.java
+++ src/test/java/edu/hw1/project1/WordGeneratorTest.java
@@ -1,15 +1,18 @@
 package edu.hw1.project1;
 
+import edu.hw1.Task5;
 import edu.project1.Word.Topic;
 import edu.project1.Word.WordGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WordGeneratorTest {
 
@@ -20,19 +23,46 @@ public class WordGeneratorTest {
             Arguments.of(Topic.Sport)
         };
     }
-    
+
+    @Test
+    @DisplayName("WordGenerator не может вернуть слово по заданной тематике")
+    void wordGeneratorCantReturnRandomWord() {
+        Topic notAvailableTopic = Topic.Country;
+        WordGenerator wordGenerator = new WordGenerator();
+        assertFalse(wordGenerator.getAvailableTopics().contains(notAvailableTopic));
+        assertThrows(IllegalArgumentException.class, () ->
+            wordGenerator.getRandomWordFromTopic(notAvailableTopic));
+    }
+
     @ParameterizedTest
     @MethodSource("availableTopics")
-    @DisplayName("WordGenerator ")
-    void wordGeneratorReturnRandomWord(Topic topic){
-        assertNotNull(WordGenerator.getRandomWordFromTopic(topic));
+    @DisplayName("WordGenerator возвращает случайное слово")
+    void wordGeneratorReturnRandomWord(Topic topic) {
+        WordGenerator wordGenerator = new WordGenerator();
+        assertNotNull(wordGenerator.getRandomWordFromTopic(topic));
     }
 
 
-    @ParameterizedTest
-    @MethodSource("availableTopics")
-    @DisplayName("WordGenerator ")
-    void wordGeneratorReturnRandomWord(Topic topic){
-        assertNotNull(WordGenerator.getRandomWordFromTopic(topic));
+
+    @Test
+    @DisplayName("Добавление нового слова в WordGenerator")
+    void addNewWordsToWordGenerator() {
+        Topic newTopic = Topic.Country;
+        Set<String> newWords = new HashSet<>(Arrays.asList("Russia", "USA", "India"));
+        WordGenerator wordGenerator = new WordGenerator();
+        assertFalse(wordGenerator.getAvailableTopics().contains(newTopic));
+        wordGenerator.addWordsToTopic(newWords, newTopic);
+        assertNotNull(wordGenerator.getRandomWordFromTopic(newTopic));
+    }
+
+    @Test
+    @DisplayName("Добавление новой тематики в WordGenerator")
+    void addNewWordToWordGenerator() {
+        String newWord = "Russia";
+        Topic newTopic = Topic.Country;
+        WordGenerator wordGenerator = new WordGenerator();
+        assertFalse(wordGenerator.getAvailableTopics().contains(newTopic));
+        wordGenerator.addNewWordToTopic(newWord, newTopic);
+        assertEquals(newWord, wordGenerator.getRandomWordFromTopic(newTopic));
     }
 }
