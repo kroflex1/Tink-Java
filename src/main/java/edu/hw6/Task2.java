78d8a806cecdd9ad2546371e21623157d957d721
Artyom Sokolov <15012003artem@mail.ru>
2023-11-14 22:17:04 +0500

    Complete Task2 and tests

--- src/main/java/edu/hw6/Task2.java
+++ src/main/java/edu/hw6/Task2.java
@@ -1,29 +1,34 @@
 package edu.hw6;
 
-import org.jetbrains.annotations.Nullable;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
-import java.util.Set;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.jetbrains.annotations.Nullable;
 
 public class Task2 {
-    private static final String COPY_FLAG = " — копия ";
+    private static final String COPY_FLAG = " — копия";
+    private static final String FILE_NAME_SEPARATOR = "\\.";
 
     public static void cloneFile(Path path) {
         if (!Files.exists(path)) {
             throw new IllegalArgumentException("Path to the file is incorrectly");
         }
         int maxCopyIndex = getMaxCopyIndex(path);
-        String pathToCopy =
-            path.getParent().toString() + removeExtensionFromFilename(path.getFileName()) + COPY_FLAG + "(" +
-                maxCopyIndex + ")" + getFileExtension(path);
+        String pathToCopy = String.format(
+            "%s/%s%s%s%s",
+            path.getParent().toString(),
+            removeExtensionFromFilename(path.getFileName()),
+            COPY_FLAG,
+            maxCopyIndex == 0 ? "" : " (" + (maxCopyIndex + 1) + ")",
+            getFileExtension(path)
+        );
+
         try {
             Files.copy(path, Paths.get(pathToCopy));
         } catch (IOException e) {
@@ -32,11 +37,13 @@ public class Task2 {
     }
 
     private static int getMaxCopyIndex(Path pathToFile) {
-        try (Stream<Path> stream = Files.list(pathToFile)) {
-            return stream
+
+        try (Stream<Path> files = Files.list(pathToFile.getParent())) {
+            var x = files
                 .filter(file -> !Files.isDirectory(file))
-                .mapToInt(currentPath -> tryGetIndexOfCopyFile(pathToFile, currentPath))
-                .max().orElse(0);
+                .map(currentPath -> tryGetIndexOfCopyFile(pathToFile, currentPath))
+                .filter(Objects::nonNull).toList();
+            return x.stream().max(Comparator.comparingInt(value -> value)).orElse(0);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -45,22 +52,26 @@ public class Task2 {
     @Nullable
     private static Integer tryGetIndexOfCopyFile(Path source, Path intendedCopyFile) {
         String sourceFileNameWithoutExtension = removeExtensionFromFilename(source);
-        Pattern pattern = Pattern.compile(sourceFileNameWithoutExtension + COPY_FLAG + "(\\((\\d+)\\))*");
-        Matcher matcher = pattern.matcher(removeExtensionFromFilename(intendedCopyFile));
+        Pattern pattern = Pattern.compile(sourceFileNameWithoutExtension + COPY_FLAG + "( \\((\\d+)\\))*");
+        Matcher matcher = pattern.matcher(removeExtensionFromFilename(intendedCopyFile.getFileName()));
         if (matcher.matches()) {
-            return matcher.group(1) == null ? 1 : Integer.parseInt(matcher.group(1));
+            return matcher.group(2) == null ? 1 : Integer.parseInt(matcher.group(2));
         }
         return null;
     }
 
     private static String removeExtensionFromFilename(Path file) {
-        String[] parts = file.getFileName().toString().split("\\.");
+        String[] parts = file.getFileName().toString().split(FILE_NAME_SEPARATOR);
         return parts[0];
     }
 
     private static String getFileExtension(Path file) {
-        String[] parts = file.getFileName().toString().split("\\.");
-        return parts[1];
+        String[] parts = file.getFileName().toString().split(FILE_NAME_SEPARATOR);
+        return "." + parts[1];
+    }
+
+    private Task2() {
+
     }
 
 }
--- src/test/java/edu/hw6/Task2Test.java
+++ src/test/java/edu/hw6/Task2Test.java
@@ -7,18 +7,22 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class Task2Test {
 
-    private static final Path ROOT = Paths.get(Paths.get("").toAbsolutePath().toString(), "/src/test/java/edu/hw6");
+    private static final Path ROOT =
+        Paths.get(Paths.get("").toAbsolutePath().toString(), "/src/test/java/edu/hw6/Task2TestDirectory");
 
-    @BeforeEach
+    @AfterEach
     void removeAllFilesInDirectory() {
         try (Stream<Path> stream = Files.list(ROOT)) {
             List<Path> paths = stream.toList();
@@ -31,37 +35,52 @@ public class Task2Test {
     }
 
     static Stream<Arguments> files() {
-        Path origin = Paths.get(ROOT.toString(), "/Tinkoff Bank Biggest Secret.txt");
+        Path origin = Paths.get(ROOT.toString(), "/Tinkoff.txt");
 
         List<Path> firstStartFiles = List.of(origin);
         int firstNumberOfCopies = 3;
-        List<Path> firstExcept = new ArrayList<>();
-        firstExcept.add(copyPath(origin, 1));
-        firstExcept.add(copyPath(origin, 2));
-        firstExcept.add(copyPath(origin, 3));
+        List<Path> firstExcept =
+            new ArrayList<>(List.of(copyPath(origin, 1), copyPath(origin, 2), copyPath(origin, 3)));
         firstExcept.addAll(firstStartFiles);
 
         List<Path> secondStartFiles = List.of(origin, copyPath(origin, 7), copyPath(origin, 8));
         int secondNumberOfCopies = 1;
-        List<Path> secondExcept = new ArrayList<>() {{
-            add(copyPath(origin, 9));
-        }};
+        List<Path> secondExcept = new ArrayList<>(List.of(copyPath(origin, 9)));
+        secondExcept.addAll(secondStartFiles);
+
+        List<Path> thirdStartFiles = List.of(origin,
+            Paths.get(ROOT.toString(), "/Tinkoff deception.txt"),
+            Paths.get(ROOT.toString(), "/someFile.txt")
+        );
+        int thirdNumberOfCopies = 3;
+        List<Path> thirdExcept =
+            new ArrayList<>(List.of(copyPath(origin, 1), copyPath(origin, 2), copyPath(origin, 3)));
         secondExcept.addAll(secondStartFiles);
 
         return Stream.of(
             arguments(origin, firstStartFiles, firstNumberOfCopies, firstExcept),
-            arguments(origin, secondStartFiles, secondNumberOfCopies, secondExcept)
+            arguments(origin, secondStartFiles, secondNumberOfCopies, secondExcept),
+            arguments(origin, thirdStartFiles, thirdNumberOfCopies, thirdExcept)
         );
     }
 
     @ParameterizedTest
     @MethodSource("files")
+    @DisplayName("Создание копий файла")
     void testCloneFiles(Path origin, List<Path> startFiles, int numberOfCopies, List<Path> except) {
         createFiles(startFiles);
         for (int i = 0; i < numberOfCopies; ++i) {
             Task2.cloneFile(origin);
         }
         assertTrue(isFilesInDirectory(except));
+    }
+
+    @Test
+    @DisplayName("Вызов ошибки при попытке клонировать несуществующий файл")
+    void testNotExistFile() {
+        Path origin = Paths.get(ROOT.toString(), "/Tinkoff.txt");
+        assertThrows(IllegalArgumentException.class, () ->
+            Task2.cloneFile(origin));
     }
 
     private static boolean isFilesInDirectory(List<Path> paths) {
@@ -74,7 +93,7 @@ public class Task2Test {
     }
 
     private static Path copyPath(Path source, int index) {
-        String[] parts = source.getFileName().toString().split(".");
+        String[] parts = source.getFileName().toString().split("\\.");
         String fileNameWithoutExtension = parts[0];
         String extension = "." + parts[1];
         String copyInformation = index == 1 ? " — копия" : String.format(" — копия (%d)", index);
