af9444339313a5f2f042b7d48c52ccc5d55bfef4
Artyom Sokolov <15012003artem@mail.ru>
2023-11-23 19:06:25 +0500

    Add Task2 and tests

---
+++ src/main/java/edu/hw7/Task2.java
@@ -0,0 +1,21 @@
+package edu.hw7;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task2 {
+
+    public static BigInteger getFactorial(long value) {
+        List<BigInteger> digits = new ArrayList<>();
+        for (long i = 1; i <= value; ++i) {
+            digits.add(BigInteger.valueOf(i));
+        }
+        return digits.parallelStream()
+            .reduce(BigInteger.valueOf(1L), (a, b) -> a.multiply(b));
+    }
+
+    private Task2(){
+
+    }
+}
